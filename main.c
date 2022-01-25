333a67ef5a46767d9fadb637076fa6f1fad7e4b8
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:42:46 +0700

    Program untuk menuliskan "Halo Dunia", 27 Desember 2021

---
+++ main.c
@@ -0,0 +1,11 @@
+/* Penulis : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com */
+/* Program untuk menuliskan Halo Dunia! ke layar */
+
+#include <stdio.h>
+int main() {
+    /* KAMUS */
+    
+    /* ALGORITMA */
+    printf ("Halo Dunia!\n");
+    return 0;
+}
